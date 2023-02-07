ff4f2c358c0162426a406c13b72349cfee38f7a8
Gabriel Vanzuita Castellain <97920108+gvcastellain@users.noreply.github.com>
2023-02-07 14:33:01 -0300

    Add files via upload

--- Conta.java
+++ Conta.java
@@ -5,7 +5,24 @@ class Conta {
     int numero;
     String titular;
 
-    void deposita(double saldo) {
-        
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino){
+        if (saca(valor) == true) {
+            saca(valor);
+            destino.deposita(valor);
+            return true;
+        } return false;
     }
 }  
